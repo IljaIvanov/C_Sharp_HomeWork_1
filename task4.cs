fb2a2ea3ebf29793267f2cf29147fd3476b1ad76
Ilja Ivanov <iljaivv@gmail.com>
2023-04-26 23:43:04 +0300

    Solution of task4

---
+++ task4.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Программа, которая на вход принимает число N, а на выходе выдает все четные числа от 1 до N");
+Console.WriteLine("Введите число N:");
+int N = Convert.ToInt32(Console.ReadLine());
+int index = 0;
+for (index=0; index<N; index++)
+{
+    if (N==0) 
+    {
+        Console.WriteLine("Вы ввели 0");
+        break;
+    }
+    if (index%2==0) Console.WriteLine("Четное число" + index);
+}
